73266feec275a40df041601b5b3043c4e46f86e2
stevepae <stevepaemusic@gmail.com>
2021-06-23 11:33:40 -0400

    update logo

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CreateIcon from '@material-ui/icons/Create';
+
+const Header = () => {
+    return <header><h1><CreateIcon/>Notes App</h1></header>
+}
+
+export default Header;
