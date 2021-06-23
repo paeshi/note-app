f7be8c5a9eeb6a4fc5c3a25d0c5ce08010b3453b
stevepae <stevepaemusic@gmail.com>
2021-06-23 11:34:31 -0400

    changed file extension to js

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-    let date = new Date();
-    let year = date.getFullYear();
-    return <footer> <p>&copy; {year} </p></footer>
-}
-
-export default Footer;
--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HighlightIcon from '@material-ui/icons/Highlight';
-
-const Header = () => {
-    return <header><h1><HighlightIcon/>Keeper</h1></header>
-}
-
-export default Header;
